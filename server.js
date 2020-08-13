fb3d0997076bfd2aeb43496456095967c1487c47
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-13 15:10:02 +0100

    :tada: initialised server.js added router

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const path=require('path')
+////////////////////////////////
+const app=express()
+////////////////////////////////
+app.set('trust proxy',true);
+
+const PORT=5000
+app.use('/ip',require('./routes/getIp'))
+app.use('/getIpInfo',require('./routes/getInfo'))
+app.use(express.static(path.join(__dirname,'public')))
+const server=app.listen(PORT,console.log(`server running in Production mode on port ${PORT}`))
+//handle unhandled promised rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`error:${err.message}`)
+    server.close(()=>process.exit(1))
+});
